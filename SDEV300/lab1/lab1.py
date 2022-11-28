3d055a85c3ffafc6846b684338889a3ed6835320
Timothy Strickland <48166931+timwstrickland@users.noreply.github.com>
2022-11-27 22:39:23 -0500

    Created lab1
    Knocking off the python rust. Going back to my old school projects

---
+++ SDEV300/lab1/lab1.py
@@ -0,0 +1,56 @@
+import sys
+import time
+
+first_name = None
+last_name = None
+age = None
+citizen = None
+state = None
+zipcode = None
+user_selection = "yes"
+
+
+def run_user_selection():
+    global user_selection
+    user_selection = input("Would you like to continue using the voter"
+                           " registration program? Please type \"yes\" or \"no\":")
+
+
+print('****************************************************************')
+print("Welcome to the US Voter registration program")
+run_user_selection()
+if user_selection == "no".lower():
+    print("Thank you for using the voter registration program")
+    time.sleep(5)
+    sys.exit()
+while user_selection == "yes".lower():
+    first_name = input("What is your first name?")
+    last_name = input("What is your last name?")
+    age = input("What is your current age?")
+    run_user_selection()
+    if user_selection == "yes".lower():
+        pass
+    elif user_selection == "no".lower():
+        break
+    else:
+        while user_selection != "yes".lower() or "no".lower():
+            input("Please enter \"yes\" or \"no\":")
+    citizen = input("Are you a US Citizen?")
+    if citizen == "yes".lower():
+        pass
+    elif citizen == "no".lower():
+        print("You are not eligible to vote!")
+        break
+    else:
+        while citizen != "yes".lower() or "no".lower():
+            input("Please enter \"yes\" or \"no\":")
+    state = input("In which state do you live? Please use 2 letters:")
+    zipcode = input("What is your zipcode:")
+    print("Here is the information we have on you:")
+    print("Name: " + first_name + " " + last_name)
+    print("Age: " + age)
+    print("State: " + state)
+    print("Zipcode: " + zipcode)
+    break
+print("Thank you for using the voter registration system")
+time.sleep(5)
